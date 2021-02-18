5bd82551ed63b8eda8981208a5f4d96bcdd1e672
benja971 <46868460+benja971@users.noreply.github.com>
2021-02-18 13:05:01 -0400

    18/02/21

--- whatsapp.js
+++ whatsapp.js
@@ -42,12 +42,8 @@ const inscription = (pseudo, email) =>
     fetch(api + "inscription&identite=" + pseudo.trim().replace(" ", "%20") + "&mail=" + email)
         .then((res) => {
             if (!document.getElementById("inscrp_btn").classList.contains("disabled")) {
-                setTimeout(() => {
-                    document.getElementById("inscrp").innerHTML += "An email has been sent to you";
-                }, 100);
-                setTimeout(() => {
-                    showConnexPage();
-                }, 3000);
+                setTimeout(() => (document.getElementById("inscrp").innerHTML += "An email has been sent to you"), 100);
+                setTimeout(() => showConnexPage(), 3000);
             }
         })
         .catch((err) => console.error("Erreur:", err));
@@ -56,30 +52,37 @@ const activation = (cle_activation) =>
     fetch(api + "activation=" + cle_activation)
         .then((res) =>
             res.json().then((json) => {
-                if (!document.querySelector("#connex_btn").classList.contains("disabled")) {
-                    id = document.querySelector("#connex_inp1").value;
-
+                if (!document.getElementById("connex_btn").classList.contains("disabled")) {
+                    id = document.getElementById("connex_inp1").value;
                     document.getElementById("connex").classList.add("invisible");
-                    console.log("add invisible to connex");
-
-                    document.getElementById("contacts").hidden = false;
-                    setTimeout(() => {
-                        document.getElementById("connex").hidden = true;
-                    }, 500);
-
+                    showContactsPage();
                     getRelations(id);
-                    document.getElementById("locate").innerHTML = "Contacts";
+                    document.getElementById("profile").classList.add("visible");
                 }
             })
         )
         .catch((err) => console.error("Erreur:", err));
 
+const setParameters = () => {
+    fetch(api + "information&identifiant=" + id)
+        .then((res) =>
+            res.json().then((json) => {
+                console.log(json);
+                document.getElementById("user_id").innerHTML = "ID: " + json.identifiant;
+                document.getElementById("user_name").innerHTML = "Name: " + json.identite;
+                document.getElementById("user_email").innerHTML = "Email: " + json.mail;
+            })
+        )
+        .catch((err) => console.error("Erreur:", err));
+};
+
 const addRelation = (email) => {
     fetch(api + "lier&identifiant=" + id + "&mail=" + email)
         .then((res) =>
             res.json().then(() => {
-                document.getElementById("newC").hidden = true;
-                document.getElementById("contacts").hidden = false;
+                document.getElementById("newC").classList.add("invisible");
+                setTimeout(() => (document.getElementById("newC").hidden = true), 350);
+                showContactsPage();
                 getRelations(id);
             })
         )
@@ -115,13 +118,7 @@ const getRelations = (id) =>
                     nc.appendChild(n);
                     nc.appendChild(m);
                     nc.onclick = (e) => {
-                        document.getElementById("back").classList.add("visible3");
-                        document.getElementById("locate").innerHTML = e.target.innerHTML;
-                        document.getElementById("more-infos").innerHTML = e.target.id;
-                        document.getElementById("contacts").hidden = true;
-                        document.getElementById("contacts").classList.add("invisible");
-                        document.getElementById("tchat").hidden = false;
-                        document.getElementById("tchat").classList.remove("invisible");
+                        showTchatPage(e);
                         readMessage(e.target.id);
                         let i = setInterval(() => readMessage(document.getElementById("more-infos").innerHTML), 500);
                     };
@@ -145,9 +142,7 @@ const delContact = (idrel) =>
 const sendMessage = (id_relation, texte) =>
     fetch(api + "ecrire&identifiant=" + id + "&relation=" + id_relation + "&message=" + texte)
         .then((res) => {
-            // console.log(res.url);
-            res.json().then((json) => {
-                // console.log(json);
+            res.json().then(() => {
                 document.getElementById("newmsg_w").innerHTML = "";
                 readMessage(id_relation);
             });
@@ -157,12 +152,10 @@ const sendMessage = (id_relation, texte) =>
 const readMessage = (id_relation) =>
     fetch(api + "lire&identifiant=" + id + "&relation=" + id_relation)
         .then((res) => {
-            // console.log(res.url);
             res.json().then((json) => {
-                console.log(json.messages);
                 for (let m of json.messages) {
-                    console.log(m);
                     let nm = document.createElement("div");
+                    console.log(m);
                     nm.classList.add("left");
 
                     let o = document.createElement("div");
@@ -185,30 +178,125 @@ const readMessage = (id_relation) =>
 function showConnexPage() {
     setTimeout(() => {
         document.getElementById("locate").innerHTML = "Connexion";
-
         document.getElementById("inscrp").classList.add("invisible");
 
         setTimeout(() => {
             document.getElementById("inscrp").hidden = true;
             document.getElementById("connex").hidden = false;
-            setTimeout(() => {
-                document.getElementById("connex").classList.remove("invisible");
-            }, 200);
+            setTimeout(() => document.getElementById("connex").classList.remove("invisible"), 200);
         }, 500);
     }, 300);
 }
 function showNewCPage() {
+    console.log("show new contacts");
+    document.getElementById("tchat").hidden = true;
     setTimeout(() => {
-        document.getElementById("contacts").hidden = true;
+        let contactslist = [...document.querySelectorAll(".cts")];
+        document.getElementById("add_c").classList.add("invisible");
+        for (let i in contactslist) {
+            setTimeout(() => {
+                contactslist[i].classList.add("reverse");
+            }, 250 * i);
+        }
+        hideContactsPage();
+        document.getElementById("locate").innerHTML = "New contact";
+        console.log(document.getElementById("locate").innerHTML);
         document.getElementById("newC").hidden = false;
+        setTimeout(() => document.getElementById("newC").classList.remove("invisible"), 500);
         document.getElementById("newC").focus;
     }, 300);
 }
 
+function hideNewCpage() {
+    console.log("hide new contact");
+
+    setTimeout(() => {
+        document.getElementById("newC").classList.add("invisible");
+        setTimeout(() => (document.getElementById("newC").hidden = true), 1000);
+    }, 300);
+}
 function showContactsPage() {
-    document.getElementById("back").classList.remove("visible3");
-    document.getElementById("locate").innerHTML = "Contacts";
-    document.getElementById("more-infos").innerHTML = "";
-    document.getElementById("contacts").hidden = false;
-    document.getElementById("contacts").classList.remove("invisible");
+    hideNewCpage();
+    console.log("show contacts");
+    document.getElementById("contacts").classList.remove("flou");
+    let i = 0;
+    for (let c of [...document.querySelectorAll(".cts")]) {
+        setTimeout(() => {
+            c.classList.remove("reverse");
+        }, 375 * i++);
+    }
+    setTimeout(() => {
+        document.getElementById("back").classList.remove("visible");
+        document.getElementById("connex").hidden = true;
+        document.getElementById("contacts").hidden = false;
+        document.getElementById("contacts").classList.remove("invisible");
+        document.getElementById("locate").innerHTML = "Contacts";
+        document.getElementById("more-infos").innerHTML = "";
+        document.getElementById("add_c").hidden = false;
+        setTimeout(() => document.getElementById("add_c").classList.remove("invisible"), 500);
+    }, 300);
+}
+
+function hideContactsPage() {
+    console.log("hide contacts");
+
+    document.getElementById("contacts").classList.add("flou");
+    setTimeout(() => {
+        document.getElementById("back").classList.add("visible");
+        let nbrcont = document.querySelectorAll(".cts").length;
+        document.getElementById("contacts").classList.add("invisible");
+        setTimeout(() => (document.getElementById("contacts").hidden = true), 375 * nbrcont);
+        // document.getElementById("locate").innerHTML = "";
+        document.getElementById("more-infos").innerHTML = "";
+        document.getElementById("add_c").classList.add("invisible");
+        setTimeout(() => (document.getElementById("add_c").hidden = true), 1000);
+    }, 300);
+}
+
+function showTchatPage(e) {
+    for (let c of [...document.querySelectorAll(".cts")]) {
+        c.classList.add("reverse");
+    }
+    document.getElementById("back").classList.add("visible");
+    document.getElementById("locate").innerHTML = e.target.innerHTML;
+    document.getElementById("more-infos").innerHTML = e.target.id;
+    document.getElementById("contacts").hidden = true;
+    document.getElementById("contacts").classList.add("invisible");
+    document.getElementById("tchat").hidden = false;
+    document.getElementById("tchat").classList.remove("invisible");
+}
+
+function showProfilePage() {
+    document.getElementById("profilepage").hidden = false;
+
+    setTimeout(() => {
+        let contactslist = [...document.querySelectorAll(".cts")];
+        document.getElementById("add_c").classList.add("invisible");
+        for (let i in contactslist) {
+            setTimeout(() => {
+                contactslist[i].classList.add("reverse");
+            }, 250 * i);
+        }
+        console.log("show profile page");
+        hideContactsPage();
+        setParameters();
+        document.getElementById("locate").innerHTML = "Profile";
+        setTimeout(() => {
+            document.getElementById("profilepage").classList.remove("invisible");
+        }, 500 * contactslist.length);
+    }, 300);
+}
+function hideProfilePage() {
+    console.log("hide profile page");
+    document.getElementById("profilepage").classList.add("invisible");
+    setTimeout(() => {
+        document.getElementById("profilepage").hidden = true;
+    }, 400);
+    let contactslist = [...document.querySelectorAll(".cts")];
+    for (let i in contactslist) {
+        setTimeout(() => {
+            contactslist[i].classList.remove("reverse");
+        }, 250 * i);
+    }
+    showContactsPage();
 }
